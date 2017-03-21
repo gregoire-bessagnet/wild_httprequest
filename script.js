abcacb47e557c8039e9a824cf1d044066c6bb5ea
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2017-03-21 19:13:43 +0100

    added debut og GET

--- script.js
+++ script.js
@@ -1,8 +1,58 @@
-var buttonclicked = document.getElementById("button");
+var city = "";
 
-buttonclicked.onclick = retrieveCity();
+$(document).ready(function(){ 
+	     $('button').click(function(){
+		 var city = document.getElementById('citytext').value;
+         console.log("stocke");
+        request();
+        console.log("lanace la fonction");
+})});
 
-function retrieveCity(e){
-   var city = document.getElementById("citytext");
-   alert(city); 
+function request() {
+
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        console.log("ca passe");
+	    var data = JSON.parse(xhr.responseText);
+    } else {
+        console.log('Une erreur s’est produite.');
+    }  
+    };
+
+xhr.open("GET", "http://api.openweathermap.org/data/2.5/find?q=" + city + "&units=metric&APPID=c8b12079e9467ab075130e3b4a785750", true);
+xhr.send(null);
+// }};
+	    };
 };
+
+
+// var push = document.getElementById("button");
+// var buttonclicked = push.addEventListener("click", // retrieveText(), false);
+
+
+// requete HTTP
+
+
+
+// function qui affiche le reultat
+
+// function readData(sData) {
+	// var nodes = oData.getElementsByTagName("soft");
+	//var ol = document.createElement("ol"), li, cn;
+	
+	//for (var i=0, c=nodes.length; i<c; i++) {
+	//	li = document.createElement("li");
+	//	cn = document.createTextNode(nodes[i].getAttribute("name"));
+		
+	//	li.appendChild(cn);
+	//	ol.appendChild(li);
+	// }
+	
+	// document.getElementById("output").appendChild(ol);
+//};
+
+// http://www.webdesignerdepot.com/2013/04/how-to-use-local-storage-for-javascript/
+
